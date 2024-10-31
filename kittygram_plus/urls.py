ea4e045c1be64663c3a1a57aae7de8b39fe0cd75
Denis Bessonov <denibessono@yandex.ru>
2024-10-31 18:14:51 +0300

    all

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,13 +1,21 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
+
 from django.urls import include, path
 
-from rest_framework.routers import DefaultRouter
-
-from cats.views import CatViewSet
+from cats.views import CatViewSet, OwnerViewSet, LightCatViewSet
 
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
+router.register('owners', OwnerViewSet)
+router.register(r'mycats', LightCatViewSet)
 
 urlpatterns = [
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
     path('', include(router.urls)),
 ]
